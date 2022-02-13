d90d11f2029220b365bac6fc74a3e756ca6cd523
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-02-13 14:50:41 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import { getPosts } from './actions/posts'
+import thoughts from './img/thoughts.png'
+import useStyles from './styles'
+
+const App = () => {
+	const [currentId, setCurrentId] = useState(null)
+	const classes = useStyles()
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(getPosts())
+	}, [currentId, dispatch])
+
+	return (
+		<Container maxwidth="lg">
+			<AppBar className={classes.appBar} position="static" color="inherit">
+				<Typography variant="h2" align="center">Thoughts</Typography>
+				<img className={classes.image} src = {thoughts} alt="thoughts" height="60" />
+			</AppBar>
+
+			<Grow in>
+				<Container>
+					<Grid container justify="space-between" alignItems="stretch" spacing={3}>
+						<Grid item xs={12} sm={7}>
+							<Posts setCurrentId={setCurrentId} />
+						</Grid>
+					
+						<Grid item xs={12} sm={4}>
+							<Form currentId={currentId} setCurrentId={setCurrentId} />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+
+
+		</Container> 
+	)
+}
+
+export default App
